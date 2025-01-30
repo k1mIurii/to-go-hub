12d471f131593754f38781be6d6714b490c94227
Yura <kyy101000@gmail.com>
2025-01-30 14:42:43 -0600

    Project backend init

---
+++ togo-hub-backend/src/main/java/com/project/togo_hub_backend/TogoHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.togo_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogoHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogoHubBackendApplication.class, args);
+	}
+
+}
---
+++ togo-hub-backend/src/test/java/com/project/togo_hub_backend/TogoHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.togo_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogoHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
